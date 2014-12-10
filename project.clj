86a78a7ddb93af49a8ee6c65bfadddde88856a98
Michael Klishin <michael@clojurewerkz.org>
2014-12-11 00:27:06 +0300

    Back to dev version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/elephant "1.0.0-beta5"
+(defproject clojurewerkz/elephant "1.0.0-beta6-SNAPSHOT"
   :description "Stripe API client in Clojure"
   :dependencies [[org.clojure/clojure    "1.6.0"]
                  [com.stripe/stripe-java "1.19.1"]]
