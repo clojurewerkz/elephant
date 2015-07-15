44c13800fa441604ed298972b4ec752b3148cad3
Michael Klishin <michael@clojurewerkz.org>
2015-07-15 13:41:46 +0300

    Back to dev version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/elephant "1.0.0-beta13"
+(defproject clojurewerkz/elephant "1.0.0-beta14-SNAPSHOT"
   :url         "https://github.com/clojurewerkz/elephant"
   :license     {:name "Eclipse Public License"
                 :url "http://www.eclipse.org/legal/epl-v10.html"}
