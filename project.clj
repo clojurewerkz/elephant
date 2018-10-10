aad945d7e174a8dbfe25b5f59ab3b41621fa3081
Michael Klishin <michael@clojurewerkz.org>
2018-10-10 19:11:14 +0300

    Bump Clojure dependency

--- project.clj
+++ project.clj
@@ -3,12 +3,10 @@
   :license     {:name "Eclipse Public License"
                 :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Stripe API client in Clojure"
-  :dependencies [[org.clojure/clojure    "1.8.0"]
+  :dependencies [[org.clojure/clojure    "1.9.0"]
                  [com.stripe/stripe-java "2.8.0"]]
-  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha13"]]}
-             :master {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}
+  :profiles {:1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :master {:dependencies [[org.clojure/clojure "1.10.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
                    :plugins [[lein-codox "0.9.0"]]
                    :codox {:sources ["src/clojure"]}}}
